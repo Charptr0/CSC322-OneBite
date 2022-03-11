37736bef93b72a586ce90276d90e85fae5f6b3dc
Charptr0 <charlesofficial1@hotmail.com>
2022-03-11 14:57:15 -0500

    added FAQs page

--- main.py
+++ main.py
@@ -2,25 +2,41 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Create a home page
 @app.route("/")
-def home():
-    return render_template("home_page.html")
-
-# Create a route to the home page
 @app.route("/home/")
 def homePage():
+    '''
+    Route to the home page
+    '''
     return render_template("home_page.html")
     
-# Menu page start
 @app.route("/menu/")
 def menu():
+    '''
+    Route to the menu page
+    '''
     return render_template("menu.html")
-
 
 @app.route("/login/")
 def loginPage():
+    '''
+    Route to the user login page
+    '''
     return render_template('login_page.html')
+
+@app.route("/faqs")
+def faqs():
+    '''
+    Route to the FAQs page
+    '''
+    return render_template("faqs.html")
+
+@app.errorhandler(404)
+def pageNotFound(e):
+    '''
+    Route to this page is the page DNE
+    '''
+    return render_template("404.html")
 
 # Run the app
 if __name__ == "__main__":
