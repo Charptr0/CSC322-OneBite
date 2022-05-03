32914f4c38d1212afb747015b93b8ee68c5e8c2f
Jessie <Jessiesu3@gmail.com>
2022-05-03 17:50:30 -0400

    Update delivery dashboard

--- main.py
+++ main.py
@@ -253,7 +253,6 @@ def profilePage():
             return redirect('/profile/')
     return render_template("profile_page.html", user=user)
 
-
 @app.route("/orders/")
 def orders():
     '''
@@ -280,7 +279,7 @@ def dashboard():
         flash("Please Log In", category="error")
         return redirect(url_for("loginPage"))
 
-    return render_template("dashboard.html", user=user, userType=user.userType)
+    return render_template("dashboard.html", user=user, userType="manager")
 
 @app.route("/dashboard-discussions/")
 def dashboardDiscussions():
