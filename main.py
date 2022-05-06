aab8f12f8963d618b865787c88435da9fcbe1077
pegliang <liangp@gmail.com>
2022-05-05 21:10:46 -0400

    manager dashboard + customer comments page

--- main.py
+++ main.py
@@ -280,7 +280,7 @@ def dashboard():
         flash("Please Log In", category="error")
         return redirect(url_for("loginPage"))
 
-    return render_template("dashboard.html", user=user, userType=user.userType)
+    return render_template("dashboard.html", user=user, userType="manager")
 
 @app.route("/dashboard-discussions/")
 def dashboardDiscussions():
